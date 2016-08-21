26dfb281ff8f592a07363b6a925d00af9fe4fcb0
Manoj Vivek <manojvivek@Manoj-Viveks-MacBook-Pro.local>
2016-08-21 22:23:24 +0530

    Adding cli options

--- aws-ri-auditor-core/src/main/java/io/cloudthrift/awsriauditor/AwsRiAuditorCli.java
+++ aws-ri-auditor-core/src/main/java/io/cloudthrift/awsriauditor/AwsRiAuditorCli.java
@@ -1,17 +1,37 @@
 package io.cloudthrift.awsriauditor;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * @author manojvivek
  *
  */
 public class AwsRiAuditorCli {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    private static Options options;
+    private static CommandLineParser parser;
+    static {
+	options = new Options();
+	options.addOption("accessKey", true, "AWS access key id to use for auditing");
+	options.addOption("secretKey", true, "AWS secret key to use for auditing");
+	parser = new DefaultParser();
+    }
 
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	try {
+	    CommandLine commandLine = parser.parse(options, args);
 
+	} catch (ParseException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+    }
 
 }
